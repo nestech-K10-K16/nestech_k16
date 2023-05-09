399a6e2ef6fff1f19d62245c95d82f4820d8a7ac
Hao <kenaru3s@gmail.com>
2023-05-09 15:30:19 +0700

    doExercise

---
+++ js/index.js
@@ -0,0 +1,73 @@
+/**
+ * <!--
+cho chuỗi A = ‘Học Javascript Front    End tại Nestech   ,Quận 10’
+  Bài 1
+	tìm vị trí chuỗi “Javascript”
+	thay vị trí chuỗi “Javascript” thành “ReactJS”
+
+*/
+let hocJS = "Học Javascript Front    End tại Nestech   ,Quận 10";
+let viTri = hocJS.indexOf("Javascript");
+console.log(viTri);
+
+let repLace = hocJS.replace("Javascript", "ReactJS");
+console.log(repLace);
+/*
+
+  Bài 2
+  	hiển thị chuỗi A với việc bỏ khoảng trắng
+	cắt chuỗi A tại thành dấu phẩy và hiển thị cụm sau của chuỗi sau khi cắt
+-->
+ */
+let boKT = hocJS.replace(/\s/g, "");
+console.log(boKT);
+/* <!--
+Bài 3
+  cho 1 mảng A = [2,3,4,5,6,7,8,-10,30].
+  tìm số lớn nhất , số nhỏ nhất
+*/
+let a = [2, 3, 4, 5, 6, 7, 8, -10, 30];
+let maxNumber = Math.max(...a);
+let minNumber = Math.min(...a);
+console.log(maxNumber);
+console.log(minNumber);
+
+/*
+Bài 4
+	cho 1 chuỗi string = ‘Học Javascript Front End’.
+  _ hiển thị độ dài chuổi
+
+  _ nối chuỗi string với chuỗi B = ‘’ở Nesteh
+-->
+*/
+
+let stringA = "Học Javascript Front End";
+let doDai = stringA.length;
+console.log(doDai);
+
+let stringB = " ở Nestech";
+let noiChuoi = stringA + stringB;
+console.log(noiChuoi);
+
+/* bài 5 - tìm số nguyên tố, kiểu tra xem mảng đó có là dãy fibonannci không ? */
+function isPrime(number) {
+  if (number < 2) return false;
+  else
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+  return true;
+}
+
+console.log(isPrime(0));
+//////
+function isFibonacci(arr) {
+  for (let i = 2; i < arr.length; i++) {
+    if (arr[i] !== arr[i - 1] + arr[i - 2]) {
+      return false;
+    }
+  }
+  return true;
+}
