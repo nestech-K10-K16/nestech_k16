cebb8d24b962d410af67d62afb6f1b9020ad3878
Nam Nguyen <namnh98@gmail.com>
2023-05-13 16:05:28 +0700

    commit vite project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <main>
+        <section>
+          <div>header</div>
+        </section>
+        <section>
+          <div>body</div>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default App;
