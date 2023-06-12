f21d6ea4a908a1fe82722078030b8bc6c5a59464
Nam Nguyen <namnh98@gmail.com>
2023-06-12 19:30:05 +0700

    commit

--- src/components/headerNavBar/index.js
+++ src/components/headerNavBar/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ROUTE_PATH } from '../../constants/route';
 
 const HeaderNavbar = () => {
   return (
     <ul>
-      <Link to="/shop">
-        <li>Shop</li>
+      <Link to={ROUTE_PATH.HOME}>
+        <li>HOME</li>
       </Link>
       <li>
         <a href="/Project">Project</a>
