1db66662a5a67deb6d7e9fa111726aefaf19b3c6
LAPTOP-0BISPP17\ASUS <thanhdi721@gmail.com>
2023-06-18 22:31:06 +0700

    thanhdi-create__cash-register

--- src/App.js
+++ src/App.js
@@ -1,62 +1,172 @@
 import React from "react";
 import "./App.css";
-// import { Routes, Route } from "react-router-dom";
-// import Home from "./pages/home/Home";
-// import Project from "./pages/project/Project";
-// import Future from "./pages/furure/Future";
+
 const App = () => {
   return (
-    <>
-      <div>
-        {/* <div class="nav">
-          <div>
-            <ul>
-              <li>
-                <a href="/">Profile</a>
-              </li>
-              <li>
-                <a href="/Project">Project</a>
-              </li>
-              <li>
-                <a href="/Future">Future</a>
-              </li>
-            </ul>
-          </div>
+    <div>
+      {/* <div className="container">
+        <div className="screen">
+          <span className="overlay"></span>
+          <span className="input"></span>
         </div>
+        <div className="keyboard">
+          <button className="item item-1">AC</button>
+          <button className="item item-2">DEL</button>
+          <button className="item item-3">+</button>
+          <button className="item item-4">7</button>
+          <button className="item item-5">8</button>
+          <button className="item item-6">9</button>
+          <button className="item item-7">-</button>
+          <button className="item item-8">4</button>
+          <button className="item item-9">5</button>
+          <button className="item item-10">6</button>
+          <button className="item item-11">*</button>
+          <button className="item item-12">1</button>
+          <button className="item item-13">2</button>
+          <button className="item item-14">3</button>
+          <button className="item item-15">/</button>
+          <button className="item item-16">0</button>
+          <button className="item item-17">00</button>
+          <button className="item item-18">.</button>
+          <button className="item item-19">=</button>
+        </div>
+      </div> */}
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Project" element={<Project />} />
-          <Route path="/Future" element={<Future />} />
-        </Routes> */}
-        <div className="container">
-          <div className="screen">
-            <span className="input"></span>
+      <div class="calculator">
+        <form action="#" name="form">
+          <div class="result">
+            <input type="text" name="disp" id="disp" />
           </div>
-          <div className="keyboard">
-            <button className="item item-1">AC</button>
-            <button className="item item-2">DEL</button>
-            <button className="item item-3">+</button>
-            <button className="item item-4">7</button>
-            <button className="item item-5">8</button>
-            <button className="item item-6">9</button>
-            <button className="item item-7">-</button>
-            <button className="item item-8">4</button>
-            <button className="item item-9">5</button>
-            <button className="item item-10">6</button>
-            <button className="item item-11">*</button>
-            <button className="item item-12">1</button>
-            <button className="item item-13">2</button>
-            <button className="item item-14">3</button>
-            <button className="item item-15">/</button>
-            <button className="item item-16">0</button>
-            <button className="item item-17">00</button>
-            <button className="item item-18">.</button>
-            <button className="item item-19">=</button>
+          <div class="buttons">
+            <div class="row">
+              <input
+                type="button"
+                value="7"
+                name="v7"
+                onclick="val(v7.value)"
+              />
+              <input
+                type="button"
+                value="8"
+                name="v8"
+                onclick="val(v8.value)"
+              />
+              <input
+                type="button"
+                value="9"
+                name="v9"
+                onclick="val(v9.value)"
+              />
+              <input
+                type="button"
+                value="+"
+                name="add"
+                onclick="val(add.value)"
+              />
+            </div>
+            <div class="row">
+              <input
+                type="button"
+                value="4"
+                name="v4"
+                onclick="val(v4.value)"
+              />
+              <input
+                type="button"
+                value="5"
+                name="v5"
+                onclick="val(v5.value)"
+              />
+              <input
+                type="button"
+                value="6"
+                name="v6"
+                onclick="val(v6.value)"
+              />
+              <input
+                type="button"
+                value="-"
+                name="sub"
+                onclick="val(sub.value)"
+              />
+            </div>
+            <div class="row">
+              <input
+                type="button"
+                value="1"
+                name="v1"
+                onclick="val(v1.value)"
+              />
+              <input
+                type="button"
+                value="2"
+                name="v2"
+                onclick="val(v2.value)"
+              />
+              <input
+                type="button"
+                value="3"
+                name="v3"
+                onclick="val(v3.value)"
+              />
+              <input
+                type="button"
+                value="*"
+                name="mul"
+                onclick="val(mul.value)"
+              />
+            </div>
+            <div class="row">
+              <input
+                type="button"
+                value="0"
+                name="v0"
+                onclick="val(v0.value)"
+              />
+              <input
+                type="button"
+                value="."
+                name="vdot"
+                onclick="val(vdot.value)"
+              />
+              <input
+                type="button"
+                value="/"
+                name="vdiv"
+                onclick="val(vdiv.value)"
+              />
+              <input
+                type="button"
+                title="Click two times to clear result"
+                value="="
+                name="veql"
+                onclick="calculate()"
+              />
+            </div>
           </div>
-        </div>
+        </form>
       </div>
-    </>
+      <script>
+      function val(result){
+            form.disp.value = form.disp.value + result;
+        }
+        
+        function calculate(){
+            if (form.disp.value == ""){
+                alert("Plase enter numbers");
+             
+            }
+            else {
+                form.disp.value = eval (form.disp.value);
+            }
+        }
+        var btn = form.veql;
+        btn.addEventListener('dblclick', function(){
+            form.disp.value = "";
+        })
+
+      </script>
+    </div>
   );
 };
 
